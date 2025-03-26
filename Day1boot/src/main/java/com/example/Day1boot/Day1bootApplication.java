acb52c28604571a25635c2229ec9606759ff571b
shravanibendre <shravanibendre@gmail.com>
2025-03-26 17:05:49 +0530

    committed

---
+++ Day1boot/src/main/java/com/example/Day1boot/Day1bootApplication.java
@@ -0,0 +1,13 @@
+package com.example.Day1boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1bootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day1bootApplication.class, args);
+	}
+
+}
---
+++ Day1boot/src/test/java/com/example/Day1boot/Day1bootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Day1boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1bootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
